167814646fd4fba3888b1f7e1ca8434c62b1468a
John Owings <john@owings5.com>
2016-02-01 03:58:01 -0500

    Scaffolded Angular

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name rdpokerApp
+ * @description
+ * # rdpokerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('rdpokerApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rdpokerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rdpokerApp
+ */
+angular.module('rdpokerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
